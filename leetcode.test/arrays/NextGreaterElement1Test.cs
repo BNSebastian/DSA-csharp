1014e9c29e75c662da4625226a9ad0b095a0c04c
Sebastian BN <bucovala.sebastian.nicu@gmail.com>
2023-07-07 01:54:06 +0300

    update

---
+++ leetcode.test/arrays/NextGreaterElement1Test.cs
@@ -0,0 +1,40 @@
+﻿using leetcode.arrays;
+
+namespace leetcode.test.arrays
+{
+    [TestClass]
+    public class NextGreaterElement1Test
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // Arrange
+            int[] nums1 = { 4, 1, 2 };
+            int[] nums2 = { 1, 3, 4, 2 };
+
+            int[] expected = { -1, 3, -1 };
+
+            // Act
+            int[] actual = NextGreaterElement1.Solution(nums1, nums2);
+
+            // Assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            // Arrange
+            int[] nums1 = { 2, 4 };
+            int[] nums2 = { 1, 2, 3, 4 };
+
+            int[] expected = { 3, -1 };
+
+            // Act
+            int[] actual = NextGreaterElement1.Solution(nums1, nums2);
+
+            // Assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ leetcode/arrays/NextGreaterElement1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace leetcode.arrays
+{
+    public static class NextGreaterElement1
+    {
+        public static int[] Solution(int[] nums1, int[] nums2)
+        {
+            int[] output = new int[nums1.Length];
+
+            for (int i = 0; i < nums1.Length; i++)
+            {
+                int j = Array.FindIndex(nums2, x => x == nums1[i]);
+
+                for (; j < nums2.Length; j++)
+                {
+                    if (nums2[j] > nums1[i])
+                    {
+                        output[i] = nums2[j];
+                        break;
+                    }
+                }
+
+                if (j == nums2.Length)
+                {
+                    output[i] = -1;
+                }
+            }
+
+            return output;
+        }
+    }
+}
